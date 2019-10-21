512d3bb0902a1cd8a30836069876f4dfe7da3ea5
Mr.Somkid <a_u_t_o@windowslive.com>
2019-10-21 21:52:55 +0700

    Add files via upload

---
+++ cannabis/index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="th">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
+  <style>
+   
+  </style>
+</html>
+<body>
+<div class="container"> 
+<div class="jumbotron">
+  <h1 class="display-4">ลงทะเบียนเข้าร่วม โครงการวิจัยน้ำมันกัญชา</h1>
+  <p class="lead">อ.เดชา ศิริภัทร</p>
+  <hr class="my-5">
+    <ul>
+      <li>
+      <strong class="text-primary">โครงการวิจัยน้ำมันกัญชา อ.เดชา ศิริภัทร  :</strong>   โรงพยาบาลสมเด็จพระยุพราชเดชอุดม เป็น 1 ใน 22 
+      โรงพยาบาลที่เข้าร่วมงานวิจัย โดยมีกรมแพทย์แผนไทยและแพทย์ทางเลือก เป็นหน่วยงานหลักในการทำวิจัย 
+      (ผ่านการขอจริยธรรมทางการวิจัยในมนุษย์แล้ว)
+      </li>
+      <li>
+      โครงการวิจัยนี้มีห้วงเวลา กันยายน 2562 ถึง สิงหาคม 2563
+      </li>
+      <li>
+      ผู้เข้าร่วมโครงการ ยินดีให้ข้อมูลแก่ ผู้รับผิดชอบ งานวิจัย ตามความเป็นจริง
+      </li>
+      <li>
+      ผู้เข้าร่วมวิจัย <strong class="text-danger my-text">*ต้องมารับบริการด้วยตนเอง ทุกครั้ง *</strong> โดย กำหนด การมาพบแพทย์  4 ครั้ง 
+      (ครั้งที่ 1 วันรับการรักษา/ครั้งที่ 2 นัด 3 เดือน / ครั้งที่ 3 นัด 2 เดือน / ครั้งที่ 4 นัด 3 เดือน )
+      </li>
+      <li>
+      ผู้เข้าร่วมงานวิจัย เป็น <strong class="text-danger my-text">ผู้มีภูมิลำเนา ในจังหวัดอุบลราชธานี </strong>
+      </li>
+    </ul> 
+    <hr class="my-4">
+    <h1 class="display-5">สิ่งที่ต้องนำมาด้วยในคัดกรอง</h1>    
+    <ol>
+      <li>บัตรประชาชนของผู้เข้าร่วมโครงการวิจัย</li>
+      <li>ทะเบียนบ้าน (ฉบับจริง หรือ สำเนา)</li>
+      <li>สำเนาประวัติ หรือ ข้อมูล ประวัติ การวินิจฉัยและรักษาในปัจจุบัน</li>
+      <li>
+      ผลการตรวจทางห้องปฏิบัติการ ดังนี้  BUN, Cr, TP, Alb, TB, AST,  ALT, ALP, AFP <strong class="text-danger my-text">(ผลตรวจไม่เกิน 1 เดือน สามารถนำผลตรวจ มาจาก รพ.ใกล้บ้านได้)  
+      *** ผู้เข้าร่วมโครงการฯ ในอำเภอเดชอุดม  ให้ขอรับการตรวจ ก่อนวันนัด  โดย มีค่าใช้จ่ายตามจริง (ชำระเงินเอง)</strong>
+      </li>
+      <li>ยาที่ใช้ในปัจจุบัน ทั้งแผนปัจจุบัน และ แผนไทย</li>
+    </ol> 
+    <hr class="my-4"> 
+    <h1 class="display-5">คุณสมบัติของผู้เข้าร่วมโครงการวิจัยเฟส 1</h1>
+    <ul>
+      <li>อาศัยหรือมีภูมิลำเนา ในจังหวัดอุบลราชธานี</li>
+      <li>เพศชายหรือเพศหญิง   อายุตั้งแต่ 25 ปีขึ้นไป</li>
+      <li>เป็นผู้ป่วยที่มีอาการหรือเป็นโรคที่แพทย์/แพทย์แผนไทย/แพทย์แผนไทยประยุกต์ ประจำโครงการฯ ของรพร.เดชอุดม กำหนดให้ใช้น้ำมันกัญชา อ.เดชา ได้ </li>
+      <li>มีการรับรู้และสติสัมปชัญญะดี สามารถสื่อสารความหหมายได้</li>
+      <li>ลงนามในใบยินยอมเร่วมโครงการ หลังได้รับการชี้แจ้งโครงการจากผู้วิจัย</li>
+    </ul>
+    
+    <hr class="my-4"> 
+    <h1 class="display-5">กำหนดการประชุม ในวันที่ 4 – 8 พฤศจิกายน 2562</h1>
+    <strong class="text-danger my-text">หมายเหตุ ผู้มีสิทธิเข้ารับการตรวจ ต้องผ่านขั้นตอน 
+                 ตั้งแต่เวลา 09.00 น. เท่านั้น
+</strong>
+    <table >
+      <tr>
+        <td>07.30 - 09.00  น.</td>
+        <td>ตรวจเอกสาร/คิวตามการสมัคร *** ไม่รับลงทะเบียนหน้างาน***</td>
+      </tr>
+      <tr>
+        <td>09.00 – 09.30 น.</td>
+        <td>ผอ.รพร.เดชอุดม กล่าวเปิดโครงการ</td>
+      </tr>
+      <tr>
+        <td>09.30- 10.00   น.</td>
+        <td>ชี้แจงโครงการวิจัยฯ</td>
+      </tr>
+      <tr>
+        <td>10.00 – 11.00 น.</td>
+        <td>บรรยายเรื่อง น้ำมันกัญชา</td>
+      </tr>
+      <tr>
+        <td>11 .00 น.</td>
+        <td>ผู้สมัครเข้ารับการตรวจ เพื่อ เข้าร่วมโครงการวิจัย ฯ</td>
+      </tr>      
+    </table>
+    <br>
+   
+    <div class="form-check mb-2 mr-sm-2">
+    <label class="form-check-label">
+      <input class="form-check-input" type="checkbox"> ข้าพเจ้าได้อ่านครบถ้วนแล้ว
+    </label>
+  </div>
+  <a class="btn btn-success btn-lg" href="#" role="button">อ่านแล้ว</a>
+</div>
+
+<div class="line-bottom">
+  <h1>ลงทะเบียน</h1>
+</div>
+<br>
+<div class="col-md-6 col-sm-6">
+<form>
+  <div class="form-group">
+    <label for="fristname">ชื่อ</label>
+    <input type="text" class="form-control" id="fristname" placeholder="กรอกชื่อ">    
+  </div>
+  <div class="form-group">
+    <label for="lastname">นามสกุล</label>
+    <input type="text" class="form-control" id="lastname" placeholder="นามสกุล">
+  </div>
+  <div class="form-group">
+    <label for="lastname">บัตรประชาชน</label>
+    <input type="text" class="form-control" id="cid" maxlength="13" placeholder="เลขบัตรประชาชน 13 หลัก">
+  </div>
+  <div class="form-group">
+    <label for="lastname">วันเดือนปีเกิด (เป็นตัวเลข)</label>
+    <div class="my-box-date">
+      วัน
+    <input type="text" id="day" min="1" max="31" maxlength="2">
+      เดือน
+    <input type="text" id="month" min="1" max="12" maxlength="2">
+      พ.ศ.
+    <input type="text" style="width:70px" id="year" min="2464" max="2537" maxlength="4">
+    </div>
+  </div>
+
+  <hr class="my-4"> 
+  <b>โรคประจำตัว ผู้ป่วย 4 กลุ่มโรค ดังนี้</b>  
+  
+  <div class="form-group form-check">
+    <input type="radio" class="form-check-input" id="Cancer" value="โรคมะเร็งทุกชนิด / ทุกระยะ (Cancer)" name="disease">
+    <label class="form-check-label text-dark" for="Cancer">โรคมะเร็งทุกชนิด / ทุกระยะ (Cancer)</label>
+  </div>
+  <div class="form-group form-check">
+    <input type="radio" class="form-check-input" id="Epilepsy" value="โรคลมชัก(Epilepsy)" name="disease">
+    <label class="form-check-label text-dark" for="Epilepsy">โรคลมชัก(Epilepsy) </label>
+  </div>
+  <div class="form-group form-check">
+    <input type="radio" class="form-check-input" id="migrain" value="โรคปวดจากปลายประสาท หรือ โรคที่มีอาการปวดรุนแรง เช่น ไมเกรน(migrain) ปลายประสาทใบหน้าอักเสบ (trigeminal nerve pain) เป็นต้น" name="disease">
+    <label class="form-check-label text-dark" for="migrain">
+      โรคปวดจากปลายประสาท หรือ โรคที่มีอาการปวดรุนแรง เช่น ไมเกรน(migrain) ปลายประสาทใบหน้าอักเสบ (trigeminal nerve pain) เป็นต้น
+    </label>
+  </div>
+  <div class="form-group form-check">
+    <input type="radio" class="form-check-input" id="Parkinson" value="พาร์กินสัน (Parkinson’s disease)" name="disease">
+    <label class="form-check-label text-dark" for="Parkinson">พาร์กินสัน (Parkinson’s disease)</label>
+  </div>  
+ 
+  <!-- end -->
+  <!-- เลือกวันที่รับคัดกรอก -->
+  <hr class="my-4"> 
+  <b>วันที่ประสงค์เข้าคัดกรอง</b>   
+  <div class="form-group form-check">
+    <input type="radio" class="form-check-input" id="screening1" value="2019-11-4" name="screening">
+    <label class="form-check-label text-dark" for="screening1">วันที่ 4 พฤศจิกายน 2562</label>
+  </div>
+  <div class="form-group form-check">
+    <input type="radio" class="form-check-input" id="screening2" value="2019-11-5" name="screening">
+    <label class="form-check-label text-dark" for="screening2">วันที่ 5 พฤศจิกายน 2562</label>
+  </div>
+  <div class="form-group form-check">
+    <input type="radio" class="form-check-input" id="screening3" value="2019-11-6" name="screening">
+    <label class="form-check-label text-dark" for="screening3">วันที่ 6 พฤศจิกายน 2562</label>
+  </div>
+  <div class="form-group form-check">
+    <input type="radio" class="form-check-input" id="screening4" value="2019-11-7" name="screening">
+    <label class="form-check-label text-dark" for="screening4">วันที่ 7 พฤศจิกายน 2562</label>
+  </div>
+  <div class="form-group form-check">
+    <input type="radio" class="form-check-input" id="screening5" value="2019-11-8" name="screening">
+    <label class="form-check-label text-dark" for="screening5">วันที่ 8 พฤศจิกายน 2562</label>
+  </div> 
+  <!-- end -->
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</div>
+</div>
+
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+  <script type="text/javascript">
+  $( document ).ready(function() {
+      $("#cid").keyup(function(){
+        if($(this).val().length == 13){
+          //console.log($(this).val());
+          let id = $(this).val();
+          let sum = 0;
+          for(i=0; i < 12; i++){
+            sum += parseFloat(id.charAt(i))*(13-i);
+          }
+          
+          if((11-(sum%11))%10 !== parseFloat(id.charAt(12))){
+              //ถ้าเลขบัตรประชาชนไม่ถูก border: 1px solid #dc3545;
+              $(this).css("border","2px solid #dc3545");
+            }else{
+              $(this).css("border","2px solid #28a745");
+            }
+        }
+      });
+  });
+
+  function checkID(id)
+    {
+    if(id.length != 13) return false;
+    for(i=0, sum=0; i < 12; i++)
+    sum += parseFloat(id.charAt(i))*(13-i); 
+    if((11-sum%11)%10!=parseFloat(id.charAt(12)))
+    return false; 
+    return true;
+    }
+
+    function checkForm()
+    { if(!checkID(document.form1.txtID.value))
+    alert('รหัสประชาชนไม่ถูกต้อง');
+    else alert('รหัสประชาชนถูกต้อง เชิญผ่านได้');}
+</script>
+
+</body>
